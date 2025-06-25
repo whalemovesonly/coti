30f6500dde40b3ec535ae40fd5b8fda2c936966c
WhaleMovesOnly <WhaleMovesOnly@gmail.com>
2025-06-25 20:37:43 +0700

    Fix bugs

--- lib/layouts/main_layout.dart
+++ lib/layouts/main_layout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:provider/provider.dart';
 import '../theme/theme_notifier.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'pages/contract_page.dart';
-import 'pages/contract_list_page.dart';
-import 'pages/dashboard_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'pages/login_page.dart';
-import 'pages/contract_detail_page.dart';
-import 'pages/create_user_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
-import 'firebase_options.dart';
-
 import 'package:provider/provider.dart';
 import 'theme/theme_notifier.dart';
 import 'theme/themes.dart';
 import 'theme/theme_type.dart';
+import 'pages/dashboard_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
 await EasyLocalization.ensureInitialized();
-    await FirebaseAppCheck.instance.activate(
-  webProvider: ReCaptchaV3Provider('6LdGO2crAAAAAEIskiYgWu3hpjDl5sPgZiMisivD'),
-);
 
   runApp(
 
