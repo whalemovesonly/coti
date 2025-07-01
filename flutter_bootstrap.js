3ca9e1594b8d8dee071be26148a0ffacc066822d
WhaleMovesOnly <WhaleMovesOnly@gmail.com>
2025-07-01 11:33:04 +0700

    Improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "162846863"
+    serviceWorkerVersion: "114854187"
   }
 });
